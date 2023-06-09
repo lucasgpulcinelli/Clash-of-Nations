e9c1a5cc6b02ec300b183713dbd57e6df132aac6
angelobguido <angelobguido@gmail.com>
2023-06-09 19:13:24 -0300

    Implement the remaining tables

--- sql/schema.sql
+++ sql/schema.sql
@@ -50,8 +50,77 @@ CREATE TABLE Cla (
     REFERENCES Nacao(nome)
 );
 
---Alianca
---Compra com doacao
---Venda
---Personagem possui itens
---Vota em alianca
+CREATE TABLE Alianca (
+  nacao1 VARCHAR(32),
+  nacao2 VARCHAR(32),
+
+  CONSTRAINT PK_Alianca PRIMARY KEY (nacao1, nacao2),
+  CONSTRAINT FK_Alianca_nacao1 FOREIGN KEY(nacao1)
+    REFERENCES Nacao(nome),
+  CONSTRAINT FK_Alianca_nacao2 FOREIGN KEY(nacao2)
+    REFERENCES Nacao(nome),
+  CONSTRAINT CHECK_Alianca_Nacao_Igual CHECK (nacao1 != nacao2)
+);
+
+CREATE TABLE Compra_Com_Doacao(
+  personagem INT,
+--  item VARCHAR(32),
+  data TIMESTAMP DEFAULT NOW(),
+  quantidade INT NOT NULL DEFAULT 1,
+
+  CONSTRAINT PK_Compra_Com_Doacao PRIMARY KEY (personagem, item, data),
+  CONSTRAINT FK_Compra_Com_Doacao_personagem FOREIGN KEY (personagem)
+    REFERENCES Personagem(ID),
+  
+--  CONSTRAINT FK_Compra_Com_Doacao_item FOREIGN KEY (item)
+--    REFERENCES Item(nome),
+    
+
+);
+
+CREATE TABLE Venda(
+  --item VARCHAR(32),
+  vendedor INT,
+  comprador INT,
+  data TIMESTAMP NOT NULL DEFAULT NOW(),
+  valor_total NOT NULL,
+  quantidade NOT NULL DEFAULT 1,
+
+  CONSTRAINT PK_Venda PRIMARY KEY (item, vendedor, comprador),
+  CONSTRAINT FK_Venda_vendedor FOREIGN KEY (vendedor)
+    REFERENCES Personagem(ID),
+  CONSTRAINT FK_Venda_comprador FOREIGN KEY (comprador)
+    REFERENCES Personagem(ID),
+  -- CONSTRAINT FK_Venda_item FOREIGN KEY (item)
+  --   REFERENCES Item(nome),
+  
+  CONSTRAINT Check_Venda_vendedor CHECK('comerciante' IN (SELECT especializacao FROM Personagem P WHERE vendedor=P.ID))
+);
+
+CREATE TABLE Personagem_Possui_Itens(
+  personagem INT,
+  --item VARCHAR(32),
+  quantidade INT NOT NULL DEFAULT 1,
+  equipado BOOLEAN NOT NULL DEFAULT false,
+
+  CONSTRAINT PK_Personagem_Possui_Itens PRIMARY KEY (personagem, item)
+  -- CONSTRAINT FK_Personagem_Possui_Itens_item FOREIGN KEY (item)
+  --   REFERENCES Item(nome),
+  CONSTRAINT FK_Personagem_Possui_Itens_personagem FOREIGN KEY (personagem)
+    REFERENCES Personagem(ID),
+  --(CHECAR SE ITEM EH EQUIPAVEL) CONSTRAINT Check_Personagem_Possui_Itens_equipado CHECK(equipado AND () )
+
+);
+
+CREATE TABLE Vota_Em_Alianca(
+  personagem INT,
+  nacao VARCHAR(32),
+  favoravel BOOLEAN NOT NULL DEFAULT false,
+
+  CONSTRAINT PK_Vota_Em_Alianca PRIMARY KEY (personagem, nacao), 
+  CONSTRAINT FK_Vota_Em_Alianca_personagem FOREIGN KEY (personagem)
+    REFERENCES Personagem(ID),
+  
+  CONSTRAINT Check_Vota_Em_Alianca_diplomata CHECK ('diplomata' IN (SELECT especializacao FROM Personagem P WHERE personagem=P.ID))
+
+);
